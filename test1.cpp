d36fba4647b54c41904a93b5049260dbb9a696e5
Acceleratorer <noobtan77@gmail.com>
2025-10-01 08:31:01 +0700

    add my second assignments

---
+++ test1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int age; cin>>age;
+    char gender; cin>>gender;
+    if ((age>=21)&&(age<=100))
+    {
+        if ((gender=='M') || (gender=='m'))
+        {
+            cout<<"1";
+        }
+        else if ((gender=='F') || (gender=='f'))
+        {
+            cout<<"2";
+        }
+        else 
+        {
+            cout<<"I do not know why";
+        }
+    }
+    else if ((age<21)&&(age>0))
+    {
+        if ((gender=='M') || (gender=='m'))
+        {
+            cout<<"3";
+        }
+        else if ((gender=='F') || (gender=='f'))
+        {
+            cout<<"4";
+        }
+        else 
+        {
+            cout<<"I do not know why";
+        }
+    }
+    return 0;  
+}
